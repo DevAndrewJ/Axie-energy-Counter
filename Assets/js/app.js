8d09508ff60ec0a9d3105b67b2db2e4af1415a27
unknown <ingjeffreyssuarez@gmail.com>
2021-11-11 16:17:53 -0400

    First Commit

---
+++ Assets/js/app.js
@@ -0,0 +1,38 @@
+
+
+
+//Referencias html
+opponentEnergy = 3;
+turnRound = 1;
+let buttonLose = document.getElementById ("lose-Btn")
+buttonLose.addEventListener("click", energyReduce)
+
+let buttonGain = document.getElementById ("gain-Btn")
+buttonGain.addEventListener("click", energyGain)
+
+let endturnBtn = document.getElementById ("end-turn-Btn")
+endturnBtn.addEventListener("click", endTurn)
+
+document.getElementById ("energy-Count").innerHTML = opponentEnergy + "/" + "10";
+document.getElementById("round-Txt").innerHTML = "Round : " + turnRound;
+
+
+
+function energyReduce () {
+    opponentEnergy = opponentEnergy - 1;
+    document.getElementById ("energy-Count").innerHTML = opponentEnergy + "/" + "10";
+    
+}
+
+
+function energyGain () {
+    opponentEnergy = opponentEnergy + 1;
+    document.getElementById ("energy-Count").innerHTML = opponentEnergy + "/" + "10";
+}
+
+function endTurn () {
+    opponentEnergy = opponentEnergy + 2;
+    turnRound = turnRound + 1;
+    document.getElementById ("energy-Count").innerHTML = opponentEnergy + "/" + "10";
+    document.getElementById ("round-Txt").innerHTML = "Round : " + turnRound
+}
