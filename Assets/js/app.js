547ea1ce775c625106f6b291f658bd1bbdbbc309
unknown <ingjeffreyssuarez@gmail.com>
2022-07-05 11:37:54 -0400

    resetbtn on

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -4,6 +4,7 @@
 //Referencias html
 opponentEnergy = 3;
 turnRound = 1;
+
 let buttonLose = document.getElementById ("lose-Btn")
 buttonLose.addEventListener("click", energyReduce)
 
@@ -12,6 +13,9 @@ buttonGain.addEventListener("click", energyGain)
 
 let endturnBtn = document.getElementById ("end-turn-Btn")
 endturnBtn.addEventListener("click", endTurn)
+
+let buttonReset = document.getElementById("reset-game-Btn")
+buttonReset.addEventListener("click", resetGame)
 
 document.getElementById ("energy-Count").innerHTML = opponentEnergy + "/" + "10";
 document.getElementById("round-Txt").innerHTML = "Round : " + turnRound;
@@ -35,4 +39,12 @@ function endTurn () {
     turnRound = turnRound + 1;
     document.getElementById ("energy-Count").innerHTML = opponentEnergy + "/" + "10";
     document.getElementById ("round-Txt").innerHTML = "Round : " + turnRound
+}
+
+function resetGame () {
+    opponentEnergy = 3;
+    turnRound = 1;
+    document.getElementById ("energy-Count").innerHTML = opponentEnergy + "/" + "10";
+    document.getElementById("round-Txt").innerHTML = "Round : " + turnRound;
+    
 }
